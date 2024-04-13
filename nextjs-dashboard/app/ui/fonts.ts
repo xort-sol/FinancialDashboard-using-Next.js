acbd1f9399ea7ecf7eeec13cf19fc2b990cbb21d
xort-sol <tomuhammadbilal485066@gmail.com>
2024-04-13 17:55:35 +0500

    changing subset font and weight

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,7 +1,7 @@
 import { Inter, Lusitana } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
-const lusitana = Lusitana({ subsets: ['latin'] });
+const lusitana = Lusitana({ subsets: ['latin'], weight: "400" }); // Specify weight as 400
 
 export {
   inter,
