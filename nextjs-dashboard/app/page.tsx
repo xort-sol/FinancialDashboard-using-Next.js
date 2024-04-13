e6ca9ef7c229cf12fde1fb56e1c8975a37a4fd15
xort-sol <tomuhammadbilal485066@gmail.com>
2024-04-13 17:20:34 +0500

    first commit

---
+++ nextjs-dashboard/app/page.tsx
@@ -0,0 +1,34 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        <AcmeLogo />
+        
+      </div>
+      <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <div className="flex flex-col gap-4 md:flex-row">
+          
+        </div>
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+        src="/hero-mobile.png"
+        width={560}
+        height={620}
+        className="block md:hidden"
+        alt="Screenshot of the dashboard project showing mobile version"
+      />
+        </div>
+    </main>
+  );
+}
